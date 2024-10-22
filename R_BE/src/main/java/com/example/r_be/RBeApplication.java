d5c17f8a9b328d61fde535b89abee923850c75e3
Tbrs Nghh <nhham1771@gmail.com>
2024-10-22 08:32:55 +0700

    1

---
+++ R_BE/src/main/java/com/example/r_be/RBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.r_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RBeApplication.class, args);
+	}
+
+}
---
+++ R_BE/src/test/java/com/example/r_be/RBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.r_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
